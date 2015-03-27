f86f301f89e66710c8cacb7ff9485b548df57f06
Harry Rickards <rickards@mit.edu>
2015-03-26 21:48:47 -0400

    Further setup testing

--- app/config/application.rb
+++ app/config/application.rb
@@ -16,8 +16,20 @@ module Holocam
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-  end
+		# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+		# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+		# config.i18n.default_locale = :de
+
+		# Testing
+		config.generators do |g|
+			g.test_framework :rspec,
+				:fixtures => true,
+				:view_specs => false,
+				:helper_specs => false,
+				:routing_specs => false,
+				:controller_specs => true,
+				:request_specs => true
+			g.fixture_replacement :factory_girl, :dir => "spec/factories"
+		end
+	end
 end
