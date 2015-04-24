8f8c2670a2b5f872856b2a24731f3dc2bf4cad7f
Harry Rickards <rickards@mit.edu>
2015-04-24 01:35:40 +0000

    Update server name for webcaming streaming

--- server/camera/stream.sh
+++ server/camera/stream.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SERVER=rickards.mit.edu
+SERVER=studioholobot.mit.edu
 RESOLUTION=320x240
 FRAMERATE=10
 avconv -re -t 5 -f video4linux2 -s $RESOLUTION -r $FRAMERATE -i /dev/video0 -vcodec flv -f flv rtmp://$SERVER/live/video
